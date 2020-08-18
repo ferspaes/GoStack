e3d4ffae14a0a73f7d96148b0fd32bc2f8ed129a
fernando.santos <fernando.js8@outlook.com>
2020-08-18 08:06:28 -0300

    node basics 8/10

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,73 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World' });
+app.use(express.json());
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar informações do back-end
+ * POST: Criar uma informação no back-end
+ * PUT/PATCH: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ * 
+ */
+
+ /**
+  * Tipos de parâmetros:
+  * 
+  * Query Params: Filtros e paginação
+  * Route Params: Identificar recursos (Atualizar/Deletar) 
+  * Request Body: Conteúdo na hora de criar ou editar um recurso (JSON)
+  * 
+  */
+
+app.get('/projects', (request, response) => {
+    const { title, owner } = request.query;
+
+    console.log(title);
+    console.log(owner);
+
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+    ]);
 });
 
-app.listen(3333);
+app.post('/projects', (request, response) => {
+    
+    const { title, owner} = request.body;
 
+    console.log(title);
+    console.log(owner);
+    
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+
+    const { id } = request.params;
+
+    console.log(id);
+
+    return response.json([
+        'Projeto 4',
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 2',
+        'Projeto 3',
+    ]);
+});
+
+app.listen(3333, () => {
+    console.log('🚀 Back-end started!')
+});
