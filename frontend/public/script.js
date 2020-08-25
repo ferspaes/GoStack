4a89e142506c29dae5bc2a5dd5edc3efc7ee136f
Fernando de Jesus dos Santos <fernando.js8@outlook.com>
2020-08-25 08:09:03 -0300

    front end initial commit

---
+++ frontend/public/script.js
@@ -0,0 +1,6 @@
+/**
+ * Babel: Converter (transpilar) código do React para um código que o browser entenda
+ * Webpack: Para cada tipo de arquivo  (.js, .css, .pjng), eu vo converter o código de uma maneira diferente
+ * 
+ * Loaders: babel-loader, css-loader, image-loder, file-loader
+ */
---
+++ frontend/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Render, render } from 'react-dom';
+import App from './App';
+
+// JSX: HTML dentro do Javascript (Javascript XML)
+
+render(<App />, document.getElementById('app'));
